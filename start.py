01ba5c551487c74a16eca808bd9a4befaa8e211d
Alison13st <teramonster13@gmail.com>
2023-06-09 14:24:22 +0700

    print()

--- start.py
+++ start.py
@@ -1,1 +1,14 @@
-Hello
+'''
+This is how difference of each function print() usages
+'''
+import time
+print("This is my first program!")
+time.sleep(0.5)
+print('This is my first program!')
+time.sleep(0.5)
+print('10'+'10')
+time.sleep(0.5)
+print('10+10')
+time.sleep(0.5)
+print(10+10)
+time.sleep(0.5)
